fed0ffc778a177dca3ff7fae2399d13d4685d0af
fjodor-rybakov <fedryb@mail.ru>
2018-10-28 11:35:24 +0300

    test server

---
+++ testServer/src/main/java/MultiServer/MonoThreadClientHandler.java
@@ -0,0 +1,58 @@
+package MultiServer;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+
+public class MonoThreadClientHandler implements Runnable {
+    private static Socket clientDialog;
+
+    public MonoThreadClientHandler(Socket client) {
+        MonoThreadClientHandler.clientDialog = client;
+    }
+
+    @Override
+    public void run() {
+        try {
+            DataOutputStream out = new DataOutputStream(clientDialog.getOutputStream());
+            DataInputStream in = new DataInputStream(clientDialog.getInputStream());
+
+            System.out.println("DataInputStream created");
+            System.out.println("DataOutputStream  created");
+
+            while (!clientDialog.isClosed()) {
+                System.out.println("Server reading from channel");
+
+                String entry = in.readUTF();
+
+                System.out.println("READ from clientDialog message - " + entry);
+
+                if (entry.equalsIgnoreCase("quit")) {
+                    System.out.println("Client.Client initialize connections suicide ...");
+                    out.writeUTF("Server reply - " + entry + " - OK");
+                    Thread.sleep(3000);
+                    break;
+                }
+
+                System.out.println("Server try writing to channel");
+                out.writeUTF("Server reply - " + entry + " - OK");
+                System.out.println("Server Wrote message to clientDialog.");
+
+                out.flush();
+            }
+
+            System.out.println("Client.Client disconnected");
+            System.out.println("Closing connections & channels.");
+
+            in.close();
+            out.close();
+
+            clientDialog.close();
+
+            System.out.println("Closing connections & channels - DONE.");
+        } catch (IOException | InterruptedException error) {
+            error.printStackTrace();
+        }
+    }
+}
