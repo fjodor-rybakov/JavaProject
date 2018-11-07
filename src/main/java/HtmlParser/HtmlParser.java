7d9946b9bcf5f7d67befdbaf22efad303bdd94b0
lezhnina-anna <Ankemova@gmail.com>
2018-11-07 19:46:24 +0300

    Add html-file parsing

---
+++ src/main/java/HtmlParser/HtmlParser.java
@@ -0,0 +1,24 @@
+package HtmlParser;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+public class HtmlParser {
+  public static void main(String argc[]) {
+    Document htmlFile = null;
+    try {
+      htmlFile = Jsoup.parse(new File("src/main/java/HtmlParser/example.html"), "ISO-8859-1");
+      Elements links = htmlFile.select("a");
+      for (Element link : links) {
+        System.out.println(link.attr("href"));
+      }
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+  }
+}
