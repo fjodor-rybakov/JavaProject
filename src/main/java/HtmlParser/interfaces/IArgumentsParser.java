83dd9c1afc52783036da6a49b0789815ab72c374
egoshin-igor <igor.egoshin@mail.ru>
2018-12-08 16:38:15 +0300

    Парсер параметров

--- src/main/java/HtmlParser/interfaces/IArgumentsParser.java
+++ src/main/java/HtmlParser/interfaces/IArgumentsParser.java
@@ -1,4 +1,9 @@
 package htmlparser.interfaces;
 
+import htmlparser.models.ArgumentsParsedResult.ArgumentsParsedResult;
+
+import java.util.List;
+
 public interface IArgumentsParser {
+    ArgumentsParsedResult getParsed(List<String> args);
 }
---
+++ src/main/java/HtmlParser/sevices/ArgumentsParser.java
@@ -0,0 +1,66 @@
+package htmlparser.sevices;
+
+import htmlparser.interfaces.IArgumentsParser;
+import htmlparser.interfaces.IHtmlQueue;
+import htmlparser.models.ArgumentsParsedResult.ArgumentsParsedResult;
+import htmlparser.models.ArgumentsParsedResult.HtmlQueue;
+
+import java.util.List;
+
+public class ArgumentsParser implements IArgumentsParser {
+    private String outFileName = "";
+    private IHtmlQueue htmlQueue;
+
+    private enum ArgumentState {
+        Files,
+        Links,
+        OutFile,
+        NoState
+    }
+
+    ArgumentsParser() {
+        this.htmlQueue = new HtmlQueue();
+    }
+
+
+    @Override
+    public ArgumentsParsedResult getParsed(List<String> args) {
+        ArgumentState state = ArgumentState.NoState;
+        for (String arg : args) {
+            switch (arg) {
+                case "--files":
+                    state = ArgumentState.Files;
+                    break;
+                case "--links":
+                    state = ArgumentState.Links;
+                    break;
+                case "--out":
+                    state = ArgumentState.OutFile;
+                    break;
+                default:
+                    locateArgument(arg, state);
+                    break;
+            }
+        }
+
+        return new ArgumentsParsedResult(htmlQueue, outFileName);
+    }
+
+    private void locateArgument(String argumnet, ArgumentState state) {
+        switch (state) {
+            case NoState:
+                throw new IllegalArgumentException("Print so: *.exe --files|--links <link|file...> --out report.csv");
+            case Files:
+                htmlQueue.addFileName(argumnet);
+                break;
+            case Links:
+                htmlQueue.addLinkName(argumnet);
+            case OutFile:
+                if (!outFileName.equals("")) {
+                    throw new IllegalArgumentException("Out file can be only single");
+                }
+                outFileName = argumnet;
+                break;
+        }
+    }
+}
