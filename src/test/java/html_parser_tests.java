4a60724340670f671fa7a567bcc09770bb4b4f17
fjodor-rybakov <fedryb@mail.ru>
2018-12-14 21:14:08 +0300

    add folder for test and Junit 5.2

---
+++ src/test/java/html_parser_tests.java
@@ -0,0 +1,10 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class html_parser_tests {
+
+    @Test
+    public void testValidator() {
+
+    }
+}
